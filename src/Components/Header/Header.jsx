6cc0f52dbe0e0e20bc94a82ca9e8ef02e4efa800
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-02-13 09:22:54 +0330

    Imported 8 images

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,5 +1,13 @@
 import React from 'react'
 import styles from "./styles.module.css"
+import image1 from "../../assets/images/hero/1.webp"
+import image2 from "../../assets/images/hero/2.webp"
+import image3 from "../../assets/images/hero/3.webp"
+import image4 from "../../assets/images/hero/4.webp"
+import image5 from "../../assets/images/hero/5.webp"
+import image6 from "../../assets/images/hero/6.jpg"
+import image7 from "../../assets/images/hero/7.webp"
+import image8 from "../../assets/images/hero/8.jpg"
 
 function Header() {
   return (

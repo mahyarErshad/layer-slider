737dac7fbbcfd9a895338a53cc7c6aba24db866f
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-02-13 11:41:53 +0330

    Imported Body

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from "react";
+import Body from "./Components/Body/Body";
 import Header from "./Components/Header/Header";
 
 function App() {
   return (
     <>
       <Header />
+      <Body />
     </>
   );
 }
